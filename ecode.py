1d188c79ace8839a0398d6f80bbb37542fc4667b
Zongqing Hou <zongqingh@gmail.com>
2017-12-28 22:50:16 +0800

    l

--- ecode.py
+++ ecode.py
@@ -1,30 +1,36 @@
 from PIL import Image,ImageFont,ImageDraw,ImageFilter
 import random
  
- def charRandom():
-    return chr((random.randint(65,90)))
+def charRandom():
+	return chr((random.randint(65,90)))
  
 def numRandom():
-    return random.randint(0,9)
+	return str(random.randint(0,9))
  
 def colorRandom1():
-    return (random.randint(64,255),random.randint(64,255),random.randint(64,255))
+	return (random.randint(64,255),random.randint(64,255),random.randint(64,255))
  
 def colorRandom2():
-    return (random.randint(32,127),random.randint(32,127),random.randint(32,127))
+	return (random.randint(32,127),random.randint(32,127),random.randint(32,127))
+
+def create_img():
+	width = 60 * 4
+	height = 60
+	image = Image.new('RGB', (width,height), (255,255,255));
+	font = ImageFont.truetype('LemonMilk.otf',36);
  
-width = 60 * 4
-height = 60
-image = Image.new('RGB', (width,height), (255,255,255));
-font = ImageFont.truetype('LemonMilk.otf',36);
- 
-draw = ImageDraw.Draw(image)
-for x in range(width):
-    for y in range(height):
-        draw.point((x,y), fill=colorRandom1())
-         
-for t in range(4):
-    draw.text((60*t+10,10), charRandom(),font=font, fill=colorRandom2())
- 
-image = image.filter(ImageFilter.BLUR)
-image.save('code.jpg','jpeg')
+	draw = ImageDraw.Draw(image)
+	return_number = ''
+	for x in range(width):
+		for y in range(height):
+			draw.point((x,y), fill=colorRandom1())
+		 
+	for t in range(4):
+		temp = numRandom()
+		draw.text((60*t+10,10), temp,font=font, fill=colorRandom2())
+		return_number += temp
+	
+	image = image.filter(ImageFilter.BLUR)
+	path = 'hello.jpg'
+	image.save(path, 'jpeg')
+	return path, return_number
--- hello.py
+++ hello.py
@@ -1,26 +1,59 @@
-from flask import Flask
+from flask import Flask, Response, redirect
+
 import tensorflow as tf
 import numpy as np
 import sklearn as sk
 import pymysql
+import re
+import glob
+
+import sys
+import os
+import random
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def to_git():
+	return redirect('https://github.com/ZongqingHou')
 
-@app.route('/user')
-def hello():
-	return 'wow'
+@app.errorhandler(404)
+def to_stack():
+	return redirect('https://stackoverflow.com/users/5478751/hdd')
 
-@app.route('/sk')
-def main_sk:
-	pass
+@app.route('/ai/veri/<number>', methods=['GET'])
+def ai(number):
+	with connection.cursor() as cursor:
+		sql = 'select * from CHECK_VERIFICATION order by req_time desc limit 1'
+		cursor.execute(sql)
+		connection.commit()
+		row = cursor.fetchone()
+	return str(row['act_value'] == number)
 
-@app.route('/tf')
-def main_tf:
-	pass
+@app.route('/image/<type>', methods=['GET'])
+def create_cha(type):
+	if type == None:
+		return 'Come on! Let me give you some challenge'
+	elif type == 'verification':
+		import ecode
+		path, number = ecode.create_img()
+		trans = open(path, 'rb')
+		now = datetime.datetime.now()
+		now.strftime('%Y-%m-%d %H:%M:%S')
+
+		with connection.cursor() as cursor:
+			sql = 'insert into CHECK_VERIFICATION (req_time, img_path, act_value) values(%s, %s, %s)'
+			cursor.execute(sql, (now, path, number))
+			connection.commit()
+		return Response(trans, mimetype="image/jpeg")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+	connection = pymysql.connect(host='localhost',
+								 port=3306,
+								 user='root',
+								 password='kevinwill21hand',
+								 db='AI',
+								 cursorclass=pymysql.cursors.DictCursor)
+	app.run(host='0.0.0.0')
+	connection.close()
